1a5e0e236fbe34824273240668b4794bfe7098d9
Sachin <prajapatsachin7078@gmail.com>
2023-09-17 20:52:00 +0530

    home-view is created

---
+++ personal-portfolio/index.js
@@ -0,0 +1,51 @@
+ // Smooth scrolling effect for navigation links
+        const navLinks = document.querySelectorAll('.navbar a');
+
+        navLinks.forEach((link) => {
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                const targetId = link.getAttribute('href').substring(1);
+                const targetSection = document.getElementById(targetId);
+
+                if (targetSection) {
+                    window.scrollTo({
+                        top: targetSection.offsetTop,
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        });
+
+        document.addEventListener("DOMContentLoaded", function () {
+            const navbarLinks = document.querySelectorAll(".navbar a");
+            const sections = document.querySelectorAll("section");
+    
+            function setActiveLink() {
+                const scrollY = window.scrollY;
+    
+                sections.forEach((section) => {
+                    const sectionTop = section.offsetTop;
+                    const sectionBottom = sectionTop + section.clientHeight;
+                    const target = section.getAttribute("id");
+    
+                    if (scrollY >= sectionTop && scrollY <= sectionBottom) {
+                        navbarLinks.forEach((link) => {
+                            link.classList.remove("active");
+                        });
+    
+                        // Find the corresponding navbar link by data-target
+                        const correspondingLink = document.querySelector(
+                            `.navbar a[data-target="${target}"]`
+                        );
+    
+                        if (correspondingLink) {
+                            correspondingLink.classList.add("active");
+                        }
+                    }
+                });
+            }
+    
+            // Add scroll event listener
+            window.addEventListener("scroll", setActiveLink);
+        });
